d291197e8fbfddaedacd6bd787bad56f0cc4ac7a
whoisterencelee <whoisterencelee@hotmail.com>
2019-01-04 18:38:49 +0800

    Added node-persistance storage and implemented data structure

--- start.js
+++ start.js
@@ -7,13 +7,20 @@ const eventBus = require('byteballcore/event_bus');
 const validationUtils = require('byteballcore/validation_utils');
 const headlessWallet = require('headless-byteball');
 
-function help(){
-	return "Pose a question which will be sent to random set of people."
-	+ "\nGet paid to answer questions by inserting your address using (...)"
-	+ "\nOnly one question at a time to prevent spam."
-}
+const storage = require( 'node-persist' )
 
-var questions = {}
+var questions = storage.create( { dir : './questions' } ) , users = []
+async function start(){
+	await questions.init()
+	users = await questions.keys()
+	console.log( "Number of registered users " + users.length )
+}
+start()
+
+function help(){
+	return "Pose a question to real people."
+	+ "\nGet paid to answer questions by inserting your address using (...)"
+}
 
 /**
  * headless wallet is ready
@@ -25,46 +32,64 @@ eventBus.once('headless_wallet_ready', () => {
 	 * user pairs his device with the bot
 	 */
 	eventBus.on('paired', (from_address, pairing_secret) => {
-		// send a geeting message
+		// send a greeting message
 		const device = require('byteballcore/device.js');
 		device.sendMessageToDevice(from_address, 'text', "Time to incentivize Q&A! Type 'help' anytime.")
 		device.sendMessageToDevice(from_address, 'text', help() )
+
+		questions.getItem( from_address ).then( existing => { if( existing === undefined ){ 
+			questions.setItem( from_address , null )
+			users.push( from_address )
+		} } )
 	});
 
 	/**
 	 * user sends message to the bot
 	 */
-	eventBus.on('text', (from_address, text) => {
+	eventBus.on('text', async (from_address, text) => {
 		// analyze the text and respond
 		text = text.trim();
 		var command = text.toLowerCase()
 
 		const device = require('byteballcore/device.js');
 
-		if( command != "help" ){
-		  
-			var exists = questions[ from_address ]
-			if( exists ) device.sendMessageToDevice( from_address , 'text' , "Waiting for answers for your question : " + exists.text )
+		if( command == "help" ) device.sendMessageToDevice( from_address , 'text' , help() )
+		else if( command == "boost" ){
+
+			var q = await questions.getItem( from_address )
+			if( !q ) device.sendMessageToDevice( from_address , 'text' , "Please submit a question first before using boost"  )
+
+			// bot sends text to more random sets of saved addresses based on boost 
+
+		} else if( /^@/.test( text ) ){  // Answers
+
+			console.log("Answer received" )
+
+		} else {  // Question
+
+			var q = await questions.getItem( from_address )
+			if( q ) device.sendMessageToDevice( from_address , 'text' , "Before asking another question, lets wait for the answers for your question :\n" + q.text )
 			else { 
-				questions[ from_address ] = { 
+				q = {
 					text : text ,
 					sent : [ ] ,
 					answers : [ ] ,
-					bounty : 0 ,
+					boost : 0 ,
 					time : new Date()
 					}
+
 				// bot sends text to random sets of saved addresses
+				var to_address = users[ Math.floor( Math.random() * users.length ) ]	
+				q.sent.push( to_address )
+
+				await questions.setItem( from_address , q )
+
+			//	if( to_address == from_address ) 
+				device.sendMessageToDevice( to_address , 'text' , "Question:\n" + text 
+					+ "\nSubmit an answer by starting with a '@'" )
 			}
+		}
 
-		} else if( command == "boost" ){
-
-			var exists = questions[ from_address ]
-			if( !exists ) device.sendMessageToDevice( from_address , 'text' , "Please submit a question first before using boost"  )
-
-			// bot sends text to more random sets of saved addresses based on bounty 
-
-			
-		} else device.sendMessageToDevice(from_address, 'text', help() )
 	});
 
 });
